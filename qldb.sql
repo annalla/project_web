973cc3f3756ac092a4dd5351fb53ffe57c829e8b
annalla <danghongxuyen2000@gmail.com>
2020-12-26 21:11:43 +0700

    chang data

--- qldb.sql
+++ qldb.sql
@@ -1,273 +1,182 @@
--- phpMyAdmin SQL Dump
--- version 5.0.3
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Dec 26, 2020 at 02:34 PM
--- Server version: 10.4.14-MariaDB
--- PHP Version: 7.2.34
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
+-- ----------------------------
+-- Table structure for userRefreshTokenExt
+-- ----------------------------
+DROP TABLE IF EXISTS `userRefreshTokenExt`;
+CREATE TABLE `userRefreshTokenExt` (
+  `ID` int(11) NOT NULL,
+  `refreshToken` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `rdt` datetime(6) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `qldb`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `comments`
---
-
-CREATE TABLE `comments` (
-  `ID_comment` int(11) UNSIGNED NOT NULL,
-  `f_ID` int(11) NOT NULL,
-  `CourseID` int(11) NOT NULL,
-  `content` varchar(300) COLLATE utf8_unicode_ci NOT NULL
+-- ----------------------------
+-- Table structure for users
+-- ----------------------------
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `f_ID` int(11) NOT NULL AUTO_INCREMENT,
+  `f_Username` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `f_Password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `f_Fullname` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
+  `f_Email` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `f_Type` int(2),
+  `f_Permission` int(11) NOT NULL,
+  PRIMARY KEY (`f_ID`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
--- --------------------------------------------------------
+-- ----------------------------
+-- Table structure for infor_teacher
+-- ----------------------------
+DROP TABLE IF EXISTS `infor_teacher`;
+CREATE TABLE `infor_teacher` (
+  `f_infor` int(11) NOT NULL AUTO_INCREMENT,
+  `f_ID` int(11) NOT NULL,
+  `job` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `brief` varchar(500) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`f_infor`),
+  FOREIGN KEY (`f_ID`) REFERENCES `users`(`f_ID`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
---
--- Table structure for table `courses`
---
+-- ----------------------------
+-- Table structure for aspects_level1
+-- ----------------------------
+DROP TABLE IF EXISTS `aspects_level1`;
+CREATE TABLE `aspects_level1` (
+  `ID_aspect1` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name_level1` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`ID_aspect1`)
+) ENGINE=MyISAM AUTO_INCREMENT=22 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+-- ----------------------------
+-- Records of aspects_level1
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for aspects_level2
+-- ----------------------------
+DROP TABLE IF EXISTS `aspects_level2`;
+CREATE TABLE `aspects_level2` (
+  `ID_aspect` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `ID_aspect1` int(11) unsigned NOT NULL,
+  `name_level2` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`ID_aspect`),
+  FOREIGN KEY (`ID_aspect1`) REFERENCES `aspects_level1`(`ID_aspect1`)
+) ENGINE=MyISAM AUTO_INCREMENT=22 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- ----------------------------
+-- Records of aspects_level2
+-- ----------------------------
+
+
+-- ----------------------------
+-- Table structure for courses
+-- ----------------------------
+DROP TABLE IF EXISTS `courses`;
 CREATE TABLE `courses` (
-  `CourseID` int(11) UNSIGNED NOT NULL,
-  `TeacherID` int(11) UNSIGNED NOT NULL,
+  `CourseID` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `ID_aspect` int(11) unsigned NOT NULL,
+  `TeacherID` int(11) unsigned NOT NULL,
   `small_image` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
   `big_image` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
   `description` varchar(500) COLLATE utf8_unicode_ci NOT NULL,
   `fee` decimal(15,2) NOT NULL,
   `discount` decimal(15,2) NOT NULL,
-  `time` time NOT NULL,
+  `time` TIME NOT NULL,
   `num_lecture` int(50) NOT NULL,
-  `status` int(2) NOT NULL
+  `status` int(2) NOT NULL,
+  PRIMARY KEY (`CourseID`),
+  FOREIGN KEY (`TeacherID`) REFERENCES `users`(`f_ID`)
+) ENGINE=MyISAM AUTO_INCREMENT=22 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+ALTER TABLE `courses`
+ADD FOREIGN KEY (`ID_aspect`) REFERENCES `aspects_level2`(`ID_aspect`);
+-- ----------------------------
+-- Records of courses
+-- ----------------------------
+/*
+BEGIN;
+INSERT INTO `courses` VALUES (1, 'Sách');
+COMMIT;
+*/
+
+
+-- ----------------------------
+-- Table structure for watchlists
+-- ----------------------------
+DROP TABLE IF EXISTS `watchlists`;
+CREATE TABLE `watchlists` (
+  `ID_list` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `f_ID` int(11) NOT NULL,
+  `CourseID` int(11) NOT NULL,
+  PRIMARY KEY (`ID_list`),
+  FOREIGN KEY (`f_ID`) REFERENCES `users`(`f_ID`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
--- --------------------------------------------------------
+ALTER TABLE `watchlists`
+ADD FOREIGN KEY (`CourseID`) REFERENCES `courses`(`CourseID`);
 
---
--- Table structure for table `course_join`
---
-
+-- ----------------------------
+-- Table structure for course_join
+-- ----------------------------
+DROP TABLE IF EXISTS `course_join`;
 CREATE TABLE `course_join` (
-  `ID_join` int(11) UNSIGNED NOT NULL,
+  `ID_join` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `f_ID` int(11) NOT NULL,
   `CourseID` int(11) NOT NULL,
   `status` int(2) NOT NULL,
-  `lecture` varchar(200) COLLATE utf8_unicode_ci DEFAULT NULL
+  `lecture` varchar(200),
+  PRIMARY KEY (`ID_join`),
+  FOREIGN KEY (`f_ID`) REFERENCES `users`(`f_ID`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
--- --------------------------------------------------------
+ALTER TABLE `course_join`
+ADD FOREIGN KEY (`CourseID`) REFERENCES `courses`(`CourseID`);
 
---
--- Table structure for table `evaluate`
---
-
+-- ----------------------------
+-- Table structure for evaluate
+-- ----------------------------
+DROP TABLE IF EXISTS `evaluate`;
 CREATE TABLE `evaluate` (
-  `ID_eva` int(11) UNSIGNED NOT NULL,
+  `ID_eva` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `f_ID` int(11) NOT NULL,
   `CourseID` int(11) NOT NULL,
-  `rate` int(10) NOT NULL
+  `rate` int(10) NOT NULL,
+  PRIMARY KEY (`ID_eva`),
+  FOREIGN KEY (`f_ID`) REFERENCES `users`(`f_ID`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `infor_teacher`
---
-
-CREATE TABLE `infor_teacher` (
-  `f_infor` int(11) NOT NULL,
+ALTER TABLE `evaluate`
+ADD FOREIGN KEY (`CourseID`) REFERENCES `courses`(`CourseID`);
+-- ----------------------------
+-- Table structure for comments
+-- ----------------------------
+DROP TABLE IF EXISTS `comments`;
+CREATE TABLE `comments` (
+  `ID_comment` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `f_ID` int(11) NOT NULL,
-  `job` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
-  `brief` varchar(500) COLLATE utf8_unicode_ci NOT NULL
+  `CourseID` int(11) NOT NULL,
+  `content` varchar(300) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`ID_comment`),
+  FOREIGN KEY (`f_ID`) REFERENCES `users`(`f_ID`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
--- --------------------------------------------------------
+ALTER TABLE `comments`
+ADD FOREIGN KEY (`CourseID`) REFERENCES `courses`(`CourseID`);
 
---
--- Table structure for table `lectures`
---
-
+-- ----------------------------
+-- Table structure for lectures
+-- ----------------------------
+DROP TABLE IF EXISTS `lectures`;
 CREATE TABLE `lectures` (
-  `ID_lect` int(11) UNSIGNED NOT NULL,
+  `ID_lect` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `CourseID` int(11) NOT NULL,
   `title` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
-  `link` varchar(100) COLLATE utf8_unicode_ci NOT NULL
+  `link` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`ID_lect`),
+  FOREIGN KEY (`CourseID`) REFERENCES `courses`(`CourseID`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `userrefreshtokenext`
---
-
-CREATE TABLE `userrefreshtokenext` (
-  `ID` int(11) NOT NULL,
-  `refreshToken` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `rdt` datetime(6) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
-
-CREATE TABLE `users` (
-  `f_ID` int(11) NOT NULL,
-  `f_Username` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  `f_Password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `f_Fullname` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
-  `f_Email` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  `f_Type` int(1) DEFAULT NULL,
-  `f_Permission` int(11) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `watchlists`
---
-
-CREATE TABLE `watchlists` (
-  `ID_list` int(11) UNSIGNED NOT NULL,
-  `f_ID` int(11) NOT NULL,
-  `CourseID` int(11) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `comments`
---
-ALTER TABLE `comments`
-  ADD PRIMARY KEY (`ID_comment`),
-  ADD KEY `f_ID` (`f_ID`),
-  ADD KEY `CourseID` (`CourseID`);
-
---
--- Indexes for table `courses`
---
-ALTER TABLE `courses`
-  ADD PRIMARY KEY (`CourseID`),
-  ADD KEY `TeacherID` (`TeacherID`);
-
---
--- Indexes for table `course_join`
---
-ALTER TABLE `course_join`
-  ADD PRIMARY KEY (`ID_join`),
-  ADD KEY `f_ID` (`f_ID`),
-  ADD KEY `CourseID` (`CourseID`);
-
---
--- Indexes for table `evaluate`
---
-ALTER TABLE `evaluate`
-  ADD PRIMARY KEY (`ID_eva`),
-  ADD KEY `f_ID` (`f_ID`),
-  ADD KEY `CourseID` (`CourseID`);
-
---
--- Indexes for table `infor_teacher`
---
-ALTER TABLE `infor_teacher`
-  ADD PRIMARY KEY (`f_infor`),
-  ADD KEY `f_ID` (`f_ID`);
-
---
--- Indexes for table `lectures`
---
-ALTER TABLE `lectures`
-  ADD PRIMARY KEY (`ID_lect`),
-  ADD KEY `CourseID` (`CourseID`);
-
---
--- Indexes for table `userrefreshtokenext`
---
-ALTER TABLE `userrefreshtokenext`
-  ADD PRIMARY KEY (`ID`);
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`f_ID`);
-
---
--- Indexes for table `watchlists`
---
-ALTER TABLE `watchlists`
-  ADD PRIMARY KEY (`ID_list`),
-  ADD KEY `f_ID` (`f_ID`),
-  ADD KEY `CourseID` (`CourseID`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `comments`
---
-ALTER TABLE `comments`
-  MODIFY `ID_comment` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `courses`
---
-ALTER TABLE `courses`
-  MODIFY `CourseID` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
-
---
--- AUTO_INCREMENT for table `course_join`
---
-ALTER TABLE `course_join`
-  MODIFY `ID_join` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `evaluate`
---
-ALTER TABLE `evaluate`
-  MODIFY `ID_eva` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `infor_teacher`
---
-ALTER TABLE `infor_teacher`
-  MODIFY `f_infor` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `lectures`
---
-ALTER TABLE `lectures`
-  MODIFY `ID_lect` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `f_ID` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `watchlists`
---
-ALTER TABLE `watchlists`
-  MODIFY `ID_list` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+SET FOREIGN_KEY_CHECKS = 1;
