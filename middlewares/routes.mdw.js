0e7de560e8fdebe140a7d3c7e27b91692d4e9b8f
trampham <pham1942000@gmail.com>
2020-12-29 17:56:40 +0700

    register thiếu otp

--- middlewares/routes.mdw.js
+++ middlewares/routes.mdw.js
@@ -2,7 +2,7 @@ module.exports = function(app){
     app.get('/', function (req, res) {
         res.render('home');
       });
-    // app.use('/account/', require('../routes/account.route'));
+    app.use('/account', require('../routes/account.route'));
     app.use('/courses', require('../routes/course.route'));
     // app.use('/courses/aspect', require('../routes/course.route1'));
 }
---
+++ routes/account.route.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const bcrypt = require('bcryptjs');
+
+const userModel = require('../models/user.model');
+
+const router = express.Router();
+
+
+router.get('/register', async function (req, res) {
+  res.render('vwAccount/register');
+})
+
+router.post('/register', async function (req, res) {
+  const hash = bcrypt.hashSync(req.body.f_Password, 10);
+  const user = {
+    f_Username:"qrq",
+    f_Password: hash,
+    f_Fullname: req.body.f_Fullname,
+    f_Email: req.body.f_Email,
+    f_Permission: 0,
+    f_Type: 0
+  }
+
+  await userModel.add(user);
+  res.render('vwAccount/register');
+})
+
+//kiểm tra tài khoản có sẵn qua email
+router.get('/is-available', async function (req, res) {
+  const email = req.query.user;
+  const user = await userModel.singleByEmail(email);
+  if (user === null) {
+    return res.json(true);
+  }
+
+  res.json(false);
+})
+
+
+module.exports = router;
