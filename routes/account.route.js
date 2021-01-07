86baeec49927be3ad7e63b990788b778fb2810a7
phangiahan1 <phanhan226@gmail.com>
2021-01-07 19:53:35 +0700

    login otp

--- routes/account.route.js
+++ routes/account.route.js
@@ -13,80 +13,94 @@ router.get('/login', async function (req, res) {
   // if (req.headers.referer) {
   //   req.session.retUrl = ref;
   // }
-  try{
+  try {
     const ref = req.headers.referer;
-      if(ref!=='undefined')
-      {
-        req.session.retUrl = ref;
-      }
-      res.render('vwAccount/login');
+    if (ref !== 'undefined') {
+      req.session.retUrl = ref;
+    }
+    res.render('vwAccount/login');
 
   }
   catch (err) {
     console.error(err);
     res.send('View error log at server console.');
   }
-  
+
 })
 
 router.post('/login', async function (req, res) {
-  try{
-  const user = await userModel.singleByEmail(req.body.f_Email);
-  const id=await userModel.getIdUser(req.body.f_Email);
-  const type=await userModel.getTypeUser(req.body.f_Email);
-  const permission=await userModel.getPermissionUser(req.body.f_Email);
-  const ret = bcrypt.compareSync(req.body.f_Password, user.f_Password);
-  if (ret === false) {
-    return res.render('vwAccount/login', {
-      err_message: 'Email/Mật khẩu không đúng!'
-    });
-  }
+  try {
+    const user = await userModel.singleByEmail(req.body.f_Email);
+    const id = await userModel.getIdUser(req.body.f_Email);
+    const type = await userModel.getTypeUser(req.body.f_Email);
+    const permission = await userModel.getPermissionUser(req.body.f_Email);
+    const ret = bcrypt.compareSync(req.body.f_Password, user.f_Password);
+    if (ret === false) {
+      return res.render('vwAccount/login', {
+        err_message: 'Email/Mật khẩu không đúng!'
+      });
+    }
 
-  req.session.isAuth = true;
-  req.session.authUser = user;
-  req.session.authId = id;
-  req.session.authPermission = permission;
-  req.session.authType = type;
-  req.session.cart=[];
-  if(+permission===1)
-  {
-    res.redirect('/admin/accounts');
+
+    req.session.isAuth = true;
+    req.session.authUser = user;
+    req.session.authId = id;
+    req.session.authPermission = permission;
+    req.session.authType = type;
+    req.session.cart = [];
+    if (+permission === 1) {
+      res.redirect('/admin');
+    }
+    else if (+type === 2) {
+      res.redirect('/account/profile');
+    }
+    else {
+      if (user.f_OTP === "0") {
+        req.session.isAuth = true;
+        console.log("vô đăng nhập bt");
+        let url = req.session.retUrl || '/';
+        if (req.session.retUrl === '/account/sendotp') {
+          url = '/';
+        }
+        if (req.session.retUrl === '/login') {
+          url = '/';
+        }
+         res.redirect(url);
+      } else {
+        console.log(user.f_OTP);
+        console.log("vô otp")
+        req.session.isAuth = false;
+        res.render('vwAccount/otp');
+        return;
+        //res.redirect('/account/sendotp');
+      }
+    }
   }
-  else if(+type===2)
-  {
-    res.redirect('/account/profile');
+  catch (err) {
+    console.error(err);
+    res.send('View error log at server console.');
   }
-  else
-  {
-    let url = req.session.retUrl || '/';
-  res.redirect(url);
-  }
-}
-catch (err) {
-  console.error(err);
-  res.send('View error log at server console.');
-}
 })
 
 router.post('/logout', async function (req, res) {
-  try{
-  req.session.isAuth = false;
-  req.session.authUser = null;
-  req.session.id = null;
-  req.session.permission = 0;
-  req.session.teacher = 0;
-  req.session.cart=[];
-  res.redirect(req.headers.referer);
-  }catch (err) {
+  try {
+    req.session.isAuth = false;
+    req.session.authUser = null;
+    req.session.id = null;
+    req.session.permission = 0;
+    req.session.teacher = 0;
+    req.session.cart = [];
+    res.redirect(req.headers.referer);
+  } catch (err) {
     console.error(err);
     res.send('View error log at server console.');
   }
 })
 
 router.get('/register', async function (req, res) {
-  try{
-  res.render('vwAccount/register');
-  }catch (err) {
+  try {
+    res.render('vwAccount/register');
+  } catch (err) {
     console.error(err);
     res.send('View error log at server console.');
   }
@@ -97,7 +111,7 @@ router.get('/register', async function (req, res) {
 //   const hash = bcrypt.hashSync(req.body.f_Password, 10);
 //   // console.log(hash);
 //   const user = {
-    
+
 //     f_Password: hash,
 //     f_Fullname: req.body.f_Fullname,
 //     f_Email: req.body.f_Email,
@@ -115,146 +129,146 @@ router.get('/register', async function (req, res) {
 
 //kiểm tra tài khoản có sẵn qua email
 router.get('/is-available', async function (req, res) {
-  try{
-  const email = req.query.user;
-  const user = await userModel.singleByEmail(email);
-  if (user === null) {
-    return res.json(true);
-  }
+  try {
+    const email = req.query.user;
+    const user = await userModel.singleByEmail(email);
+    if (user === null) {
+      return res.json(true);
+    }
 
-  res.json(false);
-}catch (err) {
-  console.error(err);
-  res.send('View error log at server console.');
-}
-})
+    res.json(false);
+  } catch (err) {
+    console.error(err);
+    res.send('View error log at server console.');
+  }
+});
 
 router.post('/sendOTP', async function (req, res) {
-  try{
-  function generateOTP() { 
-    // Declare a string variable  
-    // which stores all string 
-    var string = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'; 
-    let OTP = ''; 
-    // Find the length of string 
-    var len = string.length; 
-    for (let i = 0; i < 6; i++ ) { 
-        OTP += string[Math.floor(Math.random() * len)]; 
-    } 
-    return OTP; 
-  } 
-
-  var transporter = nodemailer.createTransport({
-      service: 'gmail',
-      auth:{
-          user: 'ptbtram220500@gmail.com',
-          pass:'trampham1942000@'
+  try {
+    function generateOTP() {
+      // Declare a string variable  
+      // which stores all string 
+      var string = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+      let OTP = '';
+      // Find the length of string 
+      var len = string.length;
+      for (let i = 0; i < 6; i++) {
+        OTP += string[Math.floor(Math.random() * len)];
       }
-  });
-   otp = generateOTP();
-  let mailOptions={
+      return OTP;
+    }
+
+    var transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: 'ptbtram220500@gmail.com',
+        pass: 'trampham1942000@'
+      }
+    });
+    otp = generateOTP();
+    let mailOptions = {
       from: 'ptbtram220500@gmail.com',
       to: req.body.f_Email,
-      subject:'XÁC THỰC TÀI KHOẢN EDU', 
-      text:'Mã OTP của bạn là: ',
+      subject: 'XÁC THỰC TÀI KHOẢN EDU',
+      text: 'Mã OTP của bạn là: ',
       html: otp
-  };
+    };
     const hash = bcrypt.hashSync(req.body.f_Password, 10);
     const user = {
-    f_Password: hash,
-    f_Fullname: req.body.f_Fullname,
-    f_Email: req.body.f_Email,
-    f_Permission: 0,
-    f_Type: 1,
-    f_OTP : otp
-  }
+      f_Password: hash,
+      f_Fullname: req.body.f_Fullname,
+      f_Email: req.body.f_Email,
+      f_Permission: 0,
+      f_Type: 1,
+      f_OTP: otp
+    }
     await userModel.add(user);
-  transporter.sendMail(mailOptions,function(error,info){
-      if(error){
-          console.log(error);
-      }else{
-          // console.log('Email sent: ' + info.response);
+    transporter.sendMail(mailOptions, function (error, info) {
+      if (error) {
+        console.log(error);
+      } else {
+        // console.log('Email sent: ' + info.response);
       }
-  });
-  res.render('vwAccount/otp')
-}catch (err) {
-  console.error(err);
-  res.send('View error log at server console.');
-}
+    });
+    res.render('vwAccount/otp')
+  } catch (err) {
+    console.error(err);
+    res.send('View error log at server console.');
+  }
 })
 
 router.post('/checkOTP', async function (req, res) {
-  try{
-  const otp = await userModel.getOTP(req.body.f_Email);
-  if (otp != req.body.OTP) {
-    return res.render('vwAccount/otp', {
-      err_message: 'OTP/email không đúng!'
-    });
-  }
-  else{
-    userModel.updateOTP('0',req.body.f_Email);
-    res.render('home', {
-      message: 'Đăng ký thành công, hãy tiến hành đăng nhập!'
-    });
+  try {
+    const otp = await userModel.getOTP(req.body.f_Email);
+    if (otp != req.body.OTP) {
+      return res.render('vwAccount/otp', {
+        err_message: 'OTP/email không đúng!'
+      });
     }
-  }catch (err) {
+    else {
+      userModel.updateOTP('0', req.body.f_Email);
+      res.render('home', {
+        message: 'Đăng ký thành công, hãy tiến hành đăng nhập!'
+      });
+    }
+  } catch (err) {
     console.error(err);
     res.send('View error log at server console.');
   }
 }),
 
-router.get('/profile', auth, async function (req, res) {
-  try{
-  res.render('vwAccount/profile');
-  }catch (err) {
+  router.get('/profile', auth, async function (req, res) {
+    try {
+      res.render('vwAccount/profile');
+    } catch (err) {
+      console.error(err);
+      res.send('View error log at server console.');
+    }
+  })
+
+router.post('/profile', auth, async function (req, res) {
+  try {
+    const id = req.session.authUser.f_ID;
+    if (req.body.f_Email != null) {
+      await userModel.setEmail(req.body.f_Email, id);
+    }
+    else if (req.body.f_Fullname != null) {
+      await userModel.setName(req.body.f_Fullname, id);
+    }
+    else if (req.body.f_Password != null && req.body.new_Password != null) {
+      const ret = bcrypt.compareSync(req.body.f_Password, req.session.authUser.f_Password);
+      if (ret === false) {
+        return res.render('vwAccount/profile', {
+          err_message: 'Mật khẩu không đúng!'
+        });
+      }
+      const hash = bcrypt.hashSync(req.body.new_Password, 10);
+      await userModel.setPassword(hash, id);
+    }
+    // res.render('vwAccount/profile');
+    req.session.isAuth = false;
+    req.session.authUser = null;
+    req.session.id = null;
+    req.session.permission = 0;
+    req.session.teacher = 0;
+    res.redirect(req.headers.referer);
+  } catch (err) {
     console.error(err);
     res.send('View error log at server console.');
   }
 })
 
-router.post('/profile',auth,async function(req,res){
-  try{
-  const id=req.session.authUser.f_ID;
-  if(req.body.f_Email != null){
-    await userModel.setEmail(req.body.f_Email,id);
-  }
-  else if(req.body.f_Fullname != null){
-    await userModel.setName(req.body.f_Fullname,id);
-  }
-  else if(req.body.f_Password != null && req.body.new_Password != null){
-    const ret = bcrypt.compareSync(req.body.f_Password, req.session.authUser.f_Password);
-    if (ret === false) {
-      return res.render('vwAccount/profile', {
-        err_message: 'Mật khẩu không đúng!'
-      });
-    }
-    const hash = bcrypt.hashSync(req.body.new_Password, 10);
-    await userModel.setPassword(hash,id);
-  }
-  // res.render('vwAccount/profile');
-  req.session.isAuth = false;
-  req.session.authUser = null;
-  req.session.id = null;
-  req.session.permission = 0;
-  req.session.teacher = 0;
-  res.redirect(req.headers.referer);
-}catch (err) {
-  console.error(err);
-  res.send('View error log at server console.');
-}
-})
-
 router.get('/checkPassword', async function (req, res) {
-  try{
-  const password = req.query.Password;
-  const ret = bcrypt.compareSync(password, req.session.authUser.f_Password);
-  if (ret === false) {
-    return res.json(false);
+  try {
+    const password = req.query.Password;
+    const ret = bcrypt.compareSync(password, req.session.authUser.f_Password);
+    if (ret === false) {
+      return res.json(false);
+    }
+    res.json(true);
+  } catch (err) {
+    console.error(err);
+    res.send('View error log at server console.');
   }
-  res.json(true);
-}catch (err) {
-  console.error(err);
-  res.send('View error log at server console.');
-}
 })
 module.exports = router;
